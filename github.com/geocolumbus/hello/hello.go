e1fe9537675e86555221db8d351669f2034900ac
George Campbell <geocolumbus@gmail.com>
2015-11-22 20:09:09 -0500

    First package.

--- github.com/geocolumbus/hello/hello.go
+++ github.com/geocolumbus/hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+        "fmt"
+
+        "github.com/geocolumbus/stringutil"
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+    fmt.Printf(stringutil.Reverse("\nOh no, it's backwards."))
 }
